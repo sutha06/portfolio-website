8224afe93f0bba7035b504b7a705f13e54e89485
sutha06 <suthakaran.siva0601@gmail.com>
2025-10-21 16:54:39 -0400

    scrolling animation added, fixing about me page

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
+
+// Initialize Lenis smooth scroll
+const lenis = new Lenis({
+  duration: 2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
